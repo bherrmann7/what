910424e423f7f753c55d293e93a562640a2bf3bd
Bob Herrmann <bherrmann@deng-bherr.(none)>
2009-12-22 08:26:25 -0500

    - When a new day is started, the new day header is automatically created

--- griffon-app/models/WhatModel.groovy
+++ griffon-app/models/WhatModel.groovy
@@ -1,25 +1,44 @@
-
+import java.text.SimpleDateFormat
 
 class WhatModel {
 
-    def file = new File(System.properties["user.home"]+"/.doing");
+  File file = new File(System.properties["user.home"] + "/.doing");
+  SimpleDateFormat sdf = new java.text.SimpleDateFormat("hh:mma ")
 
-    WhatModel(){
-        if (file.exists())
-            text = file.text
-        text += '\n '+new java.text.SimpleDateFormat("hh:mma ").format(new Date())
+  WhatModel() {
+    if (file.exists())
+      text = file.text
+
+    // if new day, insert new day header
+    // detect new day by seeing if now is less than last time.
+    def lines = text.split('\n')
+    def lastTime = lines[-1][1..sdf.toPattern().size() + 1]
+    println "lastTime is " + lastTime
+    Date last = sdf.parse(lastTime)
+    try {
+      Date now = sdf.parse(sdf.format(new Date()))
+      if (last.after(now)) {
+        text += '\n\n'+new SimpleDateFormat('EEEE, MMMM d').format(new Date())        
+      }
+    } catch (e) {
+      // just means last line wasn't a timestamp... no worries.
     }
 
+    text += '\n ' + sdf.format(new Date())
 
-    String text
 
-    def save = {
-        file.text = text;
-        println "Saved $text"
-    }
+  }
 
-    String getTally() {
-        println "Doing tally on "+text
-        new Tally().doTally(text);
-    }
+
+  String text
+
+  def save = {
+    file.text = text;
+    println "Saved $text"
+  }
+
+  String getTally() {
+    println "Doing tally on " + text
+    new Tally().doTally(text);
+  }
 }
