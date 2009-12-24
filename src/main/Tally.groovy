db8fbed56f0c4aa6bdf824ed6dec2abd3905e99b
Bob Herrmann <bherrmann@deng-bherr.(none)>
2009-12-24 09:01:01 -0500

    clean up

--- src/main/Tally.groovy
+++ src/main/Tally.groovy
@@ -1,61 +1,66 @@
 class Tally {
-    String doTally(file) {
-        StringBuilder sb = new StringBuilder()
-        def println = {
-            sb.append(it)
-            sb.append('\n')
+
+  int minSinceMidnight(time) {
+    def min = Integer.parseInt(time[0..1]) * 60 + Integer.parseInt(time[3..4])
+    if (time[0..1] != '12' && time[5] == 'P')
+      min += 12 * 60
+    min
+  }
+
+  StringBuilder sb = new StringBuilder()
+  def addLine = {
+    sb.append(it)
+    sb.append('\n')
+  }
+  def total = 0
+  def day = 0
+
+  String doTally(file) {
+
+    int start = -1
+    def lastTime
+
+    def currentDay
+    def matcher
+    def endWith = ' '
+
+    file.eachLine {line ->
+      matcher = line =~ /^ (\d\d:\d\d[AP]M)/
+      if (matcher) {
+        def time = matcher[0][1]
+        lastTime = time
+        if (start == -1) {
+          //addLine time
+          start = minSinceMidnight(time)
         }
-        def total = 0
-
-        def minSinceMidnight = {time ->
-            def min = Integer.parseInt(time[0..1]) * 60 + Integer.parseInt(time[3..4])
-            if (time[0..1] != '12' && time[5] == 'P')
-                min += 12 * 60
-            min
+      }
+      else {
+        if (start != -1) {
+          if (line.startsWith('s:'))
+            endWith = line.substring(2).trim()
+          def min = (minSinceMidnight(lastTime) - start)
+          def minStr = Integer.toString(min % 60)
+          if (minStr.size() == 1)
+            minStr = '0' + minStr
+          total += min
+          day += min
         }
+        start = -1
+      }
 
-//return minSinceMidnight("01:00PM")
-
-        int start = -1
-        def lastTime
-
-        def entries = [:]
-        def currentDay
-        def matcher
-
-        file.eachLine {line ->
-            matcher = line =~ /^ (\d\d:\d\d[AP]M)/
-            if (matcher) {
-//       println '** '+line
-                def time = matcher[0][1]
-                lastTime = time
-                if (start == -1) {
-                    //println time
-                    start = minSinceMidnight(time)
-                }
-            }
-            else {
-                if (start != -1) {
-                    def endWith =' '
-                    if (line.startsWith('s:'))
-                        endWith = line.substring(2).trim()
-                    def min = (minSinceMidnight(lastTime) - start)
-                    def minStr = Integer.toString(min % 60)
-                    if (minStr.size()==1)
-                      minStr = '0'+minStr
-                    println currentDay+'\t' + (int) (min / 60) + ':' + minStr +'\t'+ endWith
-                    total += min
-                }
-//       println '-- '+line
-                start = -1
-            }
-
-            matcher = line =~ /^\w+,\s\w+\s\w+/
-            if (matcher) {
-                currentDay = line
-            }
-        }
-        println "total ${total / 60} hours"
-        sb.toString()
+      matcher = line =~ /^\w+,\s\w+\s\w+/
+      if (matcher) {
+        if (currentDay)
+          addLine currentDay + '\t' + day / 60  +'\t' + endWith
+        day = 0
+        currentDay = line
+      }
+      
     }
+    if (currentDay)
+      addLine currentDay + '\t' + day / 60  +'\t' + endWith
+    
+    addLine "total ${total / 60} hours"
+    sb.toString()
+  }
 }
--- test/unit/TallyTest.groovy
+++ test/unit/TallyTest.groovy
@@ -4,26 +4,23 @@ class TallyTest extends GroovyTestCase {
 
 Monday, December 21
  09:30AM arrive
- 10:00AM weekly meetign (take aways: project urgency, other airlines interested)
  11:00AM emails...
  12:00PM lunch
 --
  12:30PM
  01:00PM
  02:00PM status
- 03:00PM called in Igor for help on the .tag that wont load correctly
  03:30PM finishing up final text
 
 Tuesday, December 22
  07:00AM email... working on fixing popup text on pricing for no extras sites
  08:00AM ...
  09:00AM
- 10:00AM trying to get a clean Orion Precheckin so I can check in my changes
  11:00AM
  12:00PM ah..  confusiion
  12:25PM lunch
 --
- 12:55PM verify precheck is running...
+ 12:55PM verify tests are running...
  01:00PM
  01:49PM
  02:00PM sit down for stand up
@@ -31,7 +28,7 @@ Tuesday, December 22
 
 Thursday, December 24
  07:00AM arrive read emails
- 08:00AM running pre-checin
+ 08:00AM running tests 
  09:00AM
 s:burp
 
